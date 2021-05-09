51876b91a349c045cfb0ef7b01481b3ef113b44a
komi-kazu <yihuangxiaofeng@gmail.com>
2021-05-09 15:05:30 +0900

    ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,17 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all.order("created_at DESC")
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:explain,:category_id,:condition_id,:delivery_fee,:prefecture_id,:sending_days,:selling_price,:image).merge(user_id: current_user.id)
+  end
 end
