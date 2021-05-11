28af61a3cd433181940772a39e2d487aa53ee312
komi-kazu <yihuangxiaofeng@gmail.com>
2021-05-11 11:11:23 +0900

    保存可否による条件分岐

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,12 +10,6 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :sending_days
 
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :condition_id, numericality: { other_than: 1 } 
-  validates :delivery_fee_id, numericality: { other_than: 1 } 
-  validates :prefecture_id, numericality: { other_than: 1 } 
-  validates :sending_days_id, numericality: { other_than: 1 } 
-
   validates :name, presence: true
   validates :explain, presence: true
   validates :category_id, presence: true
@@ -24,6 +18,13 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true
   validates :sending_days_id, presence: true
   validates :image, presence: true
+  
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :delivery_fee_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :sending_days_id, numericality: { other_than: 1 } 
+
 
   with_options presence: true, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' } do
     validates :selling_price
