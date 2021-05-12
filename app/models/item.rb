4317ba8abda0383010df365e2158aaed524ec584
komi-kazu <yihuangxiaofeng@gmail.com>
2021-05-12 14:30:41 +0900

    修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,21 +10,16 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :sending_days
 
-  validates :name, presence: true
-  validates :explain, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :delivery_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :sending_days_id, presence: true
-  validates :selling_price, presence: true
-  validates :image, presence: true
+  with_options presence: true do
+    validates :name
+    validates :explain
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :condition_id, numericality: { other_than: 1 } 
+    validates :delivery_fee_id, numericality: { other_than: 1 } 
+    validates :prefecture_id, numericality: { other_than: 1 } 
+    validates :sending_days_id, numericality: { other_than: 1 } 
+    validates :selling_price, inclusion: { in: 300..9999999 }
+    validates :image
+  end
   
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :condition_id, numericality: { other_than: 1 } 
-  validates :delivery_fee_id, numericality: { other_than: 1 } 
-  validates :prefecture_id, numericality: { other_than: 1 } 
-  validates :sending_days_id, numericality: { other_than: 1 } 
-
-  validates :selling_price, inclusion: { in: 300..9999999 }
 end
